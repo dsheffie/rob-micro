b0d5169dbb41b4fe5535aadbdbfae1a3ed429d9b
David Sheffield <sheffield.david@gmail.com>
2021-02-26 11:38:13 -0800

    round robin incq

--- codegen_amd64.hh
+++ codegen_amd64.hh
@@ -1,5 +1,28 @@
 #ifndef _codegen_amd64_
 #define _codegen_amd64_
+
+static void save_gprs(int &offs, uint8_t *buf) {
+  
+  for(int i = 0; i < 8; i++) {
+    buf[offs++] = 0x41;
+    buf[offs++] = 0x50+i;
+  }
+
+}
+
+static void restore_gprs(int &offs, uint8_t *buf) {
+  for(int i = 15; i > 7; i--) {
+    buf[offs++] = 0x41;
+    buf[offs++] = 0x50+i;    
+  }
+}
+
+static void round_robin_incq(int &offs, uint8_t *buf, int i) {
+  buf[offs++] = 0x49;
+  buf[offs++] = 0xff;
+  buf[offs++] = 0xc0 + (i % 8);
+}
+
 ubench_t make_code(uint8_t* buf, size_t buflen, int num_nops, int unroll=4) {
   assert(unroll < 128);
   union {
@@ -9,11 +32,12 @@ ubench_t make_code(uint8_t* buf, size_t buflen, int num_nops, int unroll=4) {
   //fill with nops
   memset(buf,0x90,buflen);
   //0:	48 89 d0             	mov    %rdx,%rax
-  //buf[0] = 0xcc;
+  buf[0] = 0xcc;
   //buf[1] = 0x90;
   //buf[2] = 0x90;
-
+  
   int offs = 0;
+  save_gprs(offs, buf);
   buf[offs++] = 0x48;
   buf[offs++] = 0x89;
   buf[offs++] = 0xd0;
@@ -27,7 +51,8 @@ ubench_t make_code(uint8_t* buf, size_t buflen, int num_nops, int unroll=4) {
     buf[offs++] = 0x3f;
     /* fill with nops */
     for(int i = 0; i < num_nops; i++) {
-      buf[offs++] = 0x90;
+      round_robin_incq(offs, buf, i);      
+      //buf[offs++] = 0x90;
     }
     //146:	48 8b 36             	mov    (%rsi),%rsi
     buf[offs++] = 0x48;
@@ -35,7 +60,8 @@ ubench_t make_code(uint8_t* buf, size_t buflen, int num_nops, int unroll=4) {
     buf[offs++] = 0x36;
     /* fill with nops */
     for(int i = 0; i < num_nops; i++) {
-      buf[offs++] = 0x90;
+      round_robin_incq(offs, buf, i);
+      //buf[offs++] = 0x90;
     }
     if(iters == 0) {
       //3 bytes before loop
@@ -58,9 +84,10 @@ ubench_t make_code(uint8_t* buf, size_t buflen, int num_nops, int unroll=4) {
   for(int i = 0; i < 4; i++) {
     buf[offs++] = uu.bytes[i];
   }
+  restore_gprs(offs, buf);
   //152:	c3                   	retq   
   buf[offs++] = 0xc3;
-
+  
   buf[buflen-1] = 0xc3;
   return reinterpret_cast<ubench_t>(buf);
 }
